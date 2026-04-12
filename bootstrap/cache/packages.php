7068a357e2155cde4524bbf3bd94edc94bd4005c
Bansal Lawyers <bansallawyers12@gmail.com>
2026-04-12 15:26:32 +1000

    Update buildspec.yml to use composer install for dependency management, add environment setup commands, and improve build process. Update composer.lock with new package versions and adjust service provider registrations in cache files for Laravel.

--- bootstrap/cache/packages.php
+++ bootstrap/cache/packages.php
@@ -27,21 +27,10 @@
   ),
   'laravel/reverb' => 
   array (
-    'aliases' => 
-    array (
-      'Output' => 'Laravel\\Reverb\\Output',
-    ),
     'providers' => 
     array (
       0 => 'Laravel\\Reverb\\ApplicationManagerServiceProvider',
       1 => 'Laravel\\Reverb\\ReverbServiceProvider',
-    ),
-  ),
-  'laravel/sail' => 
-  array (
-    'providers' => 
-    array (
-      0 => 'Laravel\\Sail\\SailServiceProvider',
     ),
   ),
   'laravel/sanctum' => 
